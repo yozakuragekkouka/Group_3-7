cfeb7e34af09b26b0122d4e36eff0810bd1de7b1
siroiZ <144640248+siroiZ@users.noreply.github.com>
2024-04-19 18:00:21 +0900

    1

--- Src/Nishiyama/Scene/SceneTitle.cpp
+++ Src/Nishiyama/Scene/SceneTitle.cpp
@@ -1,24 +1,33 @@
 #include "DxLib.h"
 #include "../../Common.h"
 #include "Scene.h"
+//2309405 宇賀塚暖紀
+
 #include "SceneTitle.h"
 #include "../Input/Input.h"
 
-#define TITLE_PATH "Data/Title/タイトル（仮置き用）.png"
-#define PLAY_ROGO_PATH "Data/Title/PLAY　仮置き.png"
-#define PLAY_ROGO2_PATH "Data/Title/PLAY　仮置き2.png"
+#define TITLE_PATH "Data/Title/タイトル.png"
+#define PLAY_ROGO_PATH "Data/Title/PLAY1.png"
+#define PLAY_ROGO2_PATH "Data/Title/PLAY2.png"
+#define PLAY_ROGO_OFF_PATH "Data/Title/PLAY OFF.png"
 
-int TitleHndl;
-int PlayrogoHndl;
-int Playrogo2Hndl;
-int Playrogocount=0;
+TitleInfo titleInfo;
+PlayRogoInfo playrogoInfo;
 
 //タイトル初期化
 void SceneTitle::Init()
 {
-	TitleHndl= LoadGraph(TITLE_PATH);             //タイトル画面初期化
-	PlayrogoHndl = LoadGraph(PLAY_ROGO_PATH);     //PLAYROGO初期化
-	Playrogo2Hndl = LoadGraph(PLAY_ROGO2_PATH);     //PLAYROGO2初期化
+	//タイトル処理関連
+	titleInfo.TitleHndl= LoadGraph(TITLE_PATH);         //タイトル画面初期化
+	titleInfo.PlaySceneFlag = false;                   //PLAYSCENEに行くためのフラグ
+	titleInfo.PlaySceneStopcount = 0;                //PLAYSCENEに行くまでの秒数
+	//プレイロゴ関連
+	playrogoInfo.PlayrogoHndl = LoadGraph(PLAY_ROGO_PATH);          //PLAYROGO初期化
+	playrogoInfo.Playrogo2Hndl = LoadGraph(PLAY_ROGO2_PATH);         //PLAYROGO2初期化
+	playrogoInfo.PlayrogoOffHndl= LoadGraph(PLAY_ROGO_OFF_PATH);     //PLAYROGO OFF初期化
+	playrogoInfo.Playrogocount = 0;                                  //描画する秒数(Playrogo)
+
+
 	SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_LOOP_TITLE;
 }
 
@@ -27,7 +36,19 @@ void SceneTitle::Step()
 {
 	if (Input::PressAnyKey())
 	{
-		SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_FIN_TITLE;
+		titleInfo.PlaySceneFlag = true;    //フラグを立てる
+	}
+
+	if (titleInfo.PlaySceneFlag == true)
+	{
+		titleInfo.PlaySceneStopcount++;   //カウントを増やす
+		if (titleInfo.PlaySceneStopcount <= 10) {
+			playrogoInfo.Playrogocount = 0;    //カウントを戻す
+		}
+		if (titleInfo.PlaySceneStopcount >= 100) {
+			titleInfo.PlaySceneFlag = true;
+			SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_FIN_TITLE;
+		}
 	}
 }
 
@@ -35,24 +56,28 @@ void SceneTitle::Step()
 void SceneTitle::Draw()
 {
 	//タイトル描画
-	DrawGraph(0, 0, TitleHndl, true);
+	DrawGraph(0, 0, titleInfo.TitleHndl, true);
 	//PLAYROGO描画
-	if (Playrogocount >= 0) {
-		DrawGraph(500, 550, PlayrogoHndl, true);
+	if (playrogoInfo.Playrogocount <= 49) {
+		DrawGraph(500, 550, playrogoInfo.PlayrogoHndl, true);    //一つ目を描画
 	}
-	if (Playrogocount >= 50) {
-		DrawGraph(500, 550, Playrogo2Hndl, true);
+	if (playrogoInfo.Playrogocount >= 50) {
+		DrawGraph(500, 550, playrogoInfo.Playrogo2Hndl, true);    //二つ目を描画
 	}
-	if (Playrogocount >= 100) {
-		Playrogocount = 0;
+	if (titleInfo.PlaySceneStopcount >= 1&& titleInfo.PlaySceneStopcount <= 10) {
+		DrawGraph(500, 550, playrogoInfo.PlayrogoOffHndl, true);   //クリック処理描画
 	}
-
-	Playrogocount++;
+	if (playrogoInfo.Playrogocount >= 100) {
+		playrogoInfo.Playrogocount = 0;    //カウントを戻す
+	}
+	if (titleInfo.PlaySceneFlag == false) {
+		playrogoInfo.Playrogocount++;   //カウントを増やす
+	}
 }
 
 //タイトル後処理
 void SceneTitle::Fin()
 {
-	Playrogocount = 0;
+	playrogoInfo.Playrogocount = 0;
 	SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_INIT_PLAY;
 }
--- Src/Nishiyama/Scene/SceneTitle.h
+++ Src/Nishiyama/Scene/SceneTitle.h
@@ -15,3 +15,20 @@ public:
 
 	void Fin();
 };
+
+//タイトル構造体
+struct TitleInfo
+{
+	int TitleHndl;                //タイトル画面
+	bool PlaySceneFlag;           //PLAYSCENEに行くためのフラグ
+	int PlaySceneStopcount;       //PLAYSCENEに行くまでの秒数
+};
+
+//タイトル構造体
+struct PlayRogoInfo
+{
+	int PlayrogoHndl;             //PLAYROGO
+	int Playrogo2Hndl;            //PLAYROGO2
+	int PlayrogoOffHndl;          //PLAYROGO OFF
+	int Playrogocount;            //描画する秒数(Playrogo)
+};
